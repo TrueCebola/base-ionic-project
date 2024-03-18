eae758e52dcaf4084037ae655c01458940b24e20
TrueCebola <carloseduardotdes@gmail.com>
2024-03-18 10:06:33 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rh-listas-presencas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
