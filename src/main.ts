564ed87861535b3040b4ada8fc8278cf6450b54e
TrueCebola <carloseduardotdes@gmail.com>
2024-04-04 11:13:11 -0300

    provided interceptors

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import {
   IonicRouteStrategy,
   provideIonicAngular,
 } from '@ionic/angular/standalone';
-import { PoStorageModule, PoStorageService } from '@po-ui/ng-storage';
+import { PoStorageService } from '@po-ui/ng-storage';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { provideHttpClient } from '@angular/common/http';
+import { PoModule, PoServicesModule } from '@po-ui/ng-components';
+import { AuthInterceptor } from './app/helpers/auth.interceptor';
+import { HttpRequestInterceptor } from './app/helpers/http.interceptor';
 
 if (environment.production) {
   enableProdMode();
@@ -22,6 +25,9 @@ bootstrapApplication(AppComponent, {
     provideIonicAngular(),
     provideRouter(routes),
     provideHttpClient(),
+    { provide: PoServicesModule },
+    { provide: AuthInterceptor },
+    { provide: HttpRequestInterceptor },
     {
       provide: PoStorageService,
       useValue: PoStorageService.providePoStorage(),
