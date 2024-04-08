0e2fa6b78edf0d983d18e971e0d721353a875797
TrueCebola <carloseduardotdes@gmail.com>
2024-04-08 10:47:49 -0300

    a

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,10 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'base.project',
   appName: 'base-project',
+  loggingBehavior: 'production',
   webDir: 'www/browser',
   server: {
+    androidScheme: 'siaadesv.cambuhy.com.br',
     cleartext: true,
   },
 };
